736a96afb4f2b36a12bdcc5766a25742a103686e
Bluscream <Bluscream@users.noreply.github.com>
2026-02-18 21:03:45 +0100

    54

--- info.ts
+++ info.ts
@@ -1,11 +1,11 @@
 
 export const pluginInfo = {
-    name: "Local Voice Channel Moderation",
+    name: "LocalVoiceChannelModeration",
     authors: [{
         name: "Bluscream",
         id: 97138137679027200n,
         github_username: "Bluscream",
-        twitter_username: "Bluscream"
+        twitter_username: "B1uscr34m"
     }],
     version: "1.0.0",
     description: "Automatically lowers the volume of users joining your voice channel.",
